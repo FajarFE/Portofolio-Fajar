cb535a30b12acaa842a8806ff08dd4d34537f158
FajarFE <fajarfernandi123123@gmail.com>
2023-03-30 21:25:47 +0700

    coba lagi

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import { logo,instagram,facebook,twitter,linkedin,tailwind } from "../assets"
 
 const Footer = () =>{
     return (
-        <footer className=" mx-auto container flex flex-col  h-20 items-center mb-5 p-5">
-            <div className="flex container justify-between p-5">
+        <footer className=" mx-auto container flex flex-col  h-20 items-center mb-5 p-5 justify-center">
+            <div className="flex container justify-between p-5 items-center">
             <div className="items-center text-white font-bold text-center">
                 <div className="flex flex-shrink-0 items-center text-center">
                     <img className="block h-12 w-auto lg:hidden" src={logo} alt="Your Company"/>
@@ -13,11 +13,11 @@ const Footer = () =>{
             <div className="my-2 items-center flex justify-between">
             <div className="sm:block ">
                     <div className="flex lg:space-x-8 md:space-x-1  text-center">
-                    <a href="#" className="text-white  hover:text-white px-1 py-2 text-[13px] sm:text-sm md:text-sm font-md hover:font-bold xs:py-1">Home</a>
-                    <a href="#" className="text-white  hover:text-white px-1 py-2 text-[13px] sm:text-sm md:text-sm font-md hover:font-bold xs:py-1">About</a>
-                    <a href="#" className="text-white  hover:text-white px-1 py-2 text-[13px] sm:text-sm md:text-sm font-md hover:font-bold xs:py-1">Skill</a>
-                    <a href="#" className="text-white  hover:text-white px-1 py-2 text-[13px] sm:text-sm md:text-sm font-md hover:font-bold xs:py-1">Timeline</a>
-                    <a href="#" className="text-white  hover:text-white px-1 py-2 text-[13px] sm:text-sm md:text-sm font-md hover:font-bold xs:py-1">Portofolio</a>
+                    <a href="#" className="text-white  hover:text-white px-1 py-2 text-[10px] sm:text-sm md:text-[18px] font-md hover:font-bold xs:py-1">Home</a>
+                    <a href="#" className="text-white  hover:text-white px-1 py-2 text-[10px] sm:text-sm md:text-[18px] font-md hover:font-bold xs:py-1">About</a>
+                    <a href="#" className="text-white  hover:text-white px-1 py-2 text-[10px] sm:text-sm md:text-[18px] font-md hover:font-bold xs:py-1">Skill</a>
+                    <a href="#" className="text-white  hover:text-white px-1 py-2 text-[10px] sm:text-sm md:text-[18px] font-md hover:font-bold xs:py-1">Timeline</a>
+                    <a href="#" className="text-white  hover:text-white px-1 py-2 text-[10px] sm:text-sm md:text-[18px] font-md hover:font-bold xs:py-1">Portofolio</a>
                     </div>
                 </div>
             </div>
@@ -25,7 +25,7 @@ const Footer = () =>{
             
            <div className="flex container justify-between py-4 border-[#333e4d] border-t-2">
             <div className="flex container justify-start items-center">
-            <p className="text-white text-sm xs:text-xs">
+            <p className="text-white text-sm xs:text-[8px]">
             © 2023 Create By Fajar Fernandi.
             </p>
             </div>
