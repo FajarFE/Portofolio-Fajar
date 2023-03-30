a33831bb131698d70570b5cd25348503fb3856e2
FajarFE <fajarfernandi123123@gmail.com>
2023-03-30 21:42:57 +0700

    coba lagi

--- src/styles.js
+++ src/styles.js
@@ -4,12 +4,12 @@ const styles = {
   padding: "sm:px-16 px-6 sm:py-16 py-10",
 
   heroHeadText:
-    "font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
+    "font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[20px] lg:leading-[98px] mt-2",
   heroSubText:
     "text-[#fff] font-medium lg:text-[40px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[50px]",
 
   sectionHeadText:
-    "text-white font-black md:text-[30px] sm:text-[30px] xs:text-[20px] text-[10px]",
+    "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
   sectionSubText:
     "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
   sectionHeadText1:
@@ -18,7 +18,7 @@ const styles = {
     "text-white font-black md:text-[40px] sm:text-[20px] xs:text-[10px] text-[20px]",
     sectionHeadText3:
     "text-white font-black md:text-[60px] sm:text-[20px] xs:text-[40px] text-[20px]",
-    sectionHeadText4:
+    sectionHeadText:
     "text-[#A3F7BF] font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
     
 };
