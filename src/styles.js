6a1f70f788c99ea80911700100febd62eedfb7ad
FajarFE <fajarfernandi123123@gmail.com>
2023-03-30 21:41:04 +0700

    coba lagi

--- src/styles.js
+++ src/styles.js
@@ -9,7 +9,7 @@ const styles = {
     "text-[#fff] font-medium lg:text-[40px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[50px]",
 
   sectionHeadText:
-    "text-white font-black md:text-[30px] sm:text-[30px] xs:text-[40px] text-[10px]",
+    "text-white font-black md:text-[30px] sm:text-[30px] xs:text-[20px] text-[10px]",
   sectionSubText:
     "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
   sectionHeadText1:
